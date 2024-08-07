20a2763afd75bc98b00b6eac6e892cf1b4545dd1
Jacob Templeton <jacob.templeton@gmail.com>
2024-08-06 19:01:35 -0700

    add more changes

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,13 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+// export default function Loading() {
+//   return (
+//     <div className="h-[100px] animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-center text-4xl font-black">
+//       Loading...
+//     </div>
+//   );
+// }
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-  return <p>Customer Page</p>;
+// import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import {
+  CustomersTableType,
+  FormattedCustomersTable,
+} from "@/app/lib/definitions";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <Table customers={customers} />
+    </main>
+  );
 }
